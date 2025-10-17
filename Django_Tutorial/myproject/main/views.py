53f736101600e1c774029555cb0d54dfaefeb5d3
899801 <899801@my.cwc.ac.uk>
2025-10-17 11:52:32 +0100

    Hello world

--- Django_Tutorial/myproject/main/views.py
+++ Django_Tutorial/myproject/main/views.py
@@ -1,8 +1,10 @@
 from django.http import HttpResponse
 
-def index(response):
-    return HttpResponse("<h1>Web Development with Ike!<!h1>")
+#def index(response):
+    #return HttpResponse("<h1>Web Development with Ike!<!h1>")
 
-def v1(response):
-    return HttpResponse("<h1>View 1!<!h1>")
-#hh
+#def v1(response):
+    #return HttpResponse("<h1>View 1!<!h1>")
+
+def index(response, id):
+    return HttpResponse("<h1>%s</h1>" %id)
